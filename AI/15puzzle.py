74fea9e3b5cdb9d45a52cb3b363ae6f8844f59d9
Devang Kamble <risingentropy20@gmail.com>
2021-08-29 13:42:57 +0530

    15puzzle

---
+++ AI/15puzzle.py
@@ -0,0 +1,112 @@
+# Find steps to solve 15 Puzzle Problem using Heuristic
+
+finalState = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, None]
+]
+
+def calculateHeuristic(matrix):
+  h = 0
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if matrix[i][j] != finalState[i][j]:
+        h += 1 
+  return h
+
+def possibleOptions(matrix):
+  lst = []
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if matrix[i][j] == None:
+        # Up
+        if i-1 > -1:
+          lst.append("UP")
+          
+        # Down
+        if i+1 < len(finalState):
+          lst.append("DOWN") 
+        
+        # Left
+        if j-1 > -1:
+          lst.append("LEFT")
+        
+        # Right
+        if j+1 < len(finalState[0]):
+          lst.append("RIGHT")
+  return lst
+
+def matrixUP(matrix):
+  bufferMatrix = matrix
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if bufferMatrix[i][j] == None:
+        temp = bufferMatrix[i-1][j]
+        bufferMatrix[i-1][j] = None
+        bufferMatrix[i][j] = temp
+        return bufferMatrix
+      
+def matrixDOWN(matrix):
+  bufferMatrix = matrix
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if bufferMatrix[i][j] == None:
+        temp = bufferMatrix[i+1][j]
+        bufferMatrix[i+1][j] = None
+        bufferMatrix[i][j] = temp
+        return bufferMatrix
+
+def matrixLEFT(matrix):
+  bufferMatrix = matrix
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if bufferMatrix[i][j] == None:
+        temp = bufferMatrix[i][j-1]
+        bufferMatrix[i][j-1] = None
+        bufferMatrix[i][j] = temp
+        return bufferMatrix
+      
+def matrixRIGHT(matrix):
+  bufferMatrix = matrix
+  for i in range(len(finalState)):
+    for j in range(len(finalState[i])):
+      if bufferMatrix[i][j] == None:
+        temp = bufferMatrix[i][j+1]
+        bufferMatrix[i][j+1] = None
+        bufferMatrix[i][j] = temp
+        return bufferMatrix
+
+def getAllOptionStates(matrix, typeList):
+  optionMatrices = []
+  
+  # UP
+  if "UP" in typeList:
+    optionMatrices.append(matrixUP(matrix))
+    print(matrixUP(matrix))
+  
+  #DOWN
+  if "DOWN" in typeList:
+    optionMatrices.append(matrixDOWN(matrix))
+  
+  #LEFT
+  if "LEFT" in typeList:
+    optionMatrices.append(matrixLEFT(matrix))
+  
+  #RIGHT
+  if "RIGHT" in typeList:
+    optionMatrices.append(matrixRIGHT(matrix))
+  
+  return optionMatrices
+
+problem = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, None],
+  [13, 14, 15, 12]
+]
+
+print(getAllOptionStates(problem, possibleOptions(problem)))
+
+
+
